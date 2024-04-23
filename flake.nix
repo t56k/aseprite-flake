3719ccabee6c0e9f7495c9b7bd1d3e1a650c3094
t56k <thomasnash@fastmail.com>
2024-04-23 13:16:32 +1000

    bump to 1.3.6

--- flake.nix
+++ flake.nix
@@ -16,14 +16,14 @@
         stripRoot = false;
       };
       aseprite-src = pkgs.fetchzip {
-        url = "https://github.com/aseprite/aseprite/releases/download/v1.3.2/Aseprite-v1.3.2-Source.zip";
-        hash = "sha256-d3Q442ApykU0AbmDP/Tx7WqmbG9Wb3PxCnIAnf0+VjU=";
+        url = "https://github.com/aseprite/aseprite/releases/download/v1.3.6/Aseprite-v1.3.6-Source.zip";
+        hash = "sha256-17f6pIGsOIswnyY63pjHKEEYuCo43kf25mPLBv4vQAs=";
         stripRoot = false;
       };
     in {
       packages."x86_64-linux".default = stdenv.mkDerivation {
         name = "aseprite";
-        version = "1.3.2";
+        version = "1.3.6";
         src = "${aseprite-src}";
         nativeBuildInputs = with pkgs; [
           clang
